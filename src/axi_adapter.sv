349e281870606df9e2e1bf933ba7e5adfdef544d
Moritz Schneider <scmoritz@student.ethz.ch>
2018-08-16 16:32:37 +0200

    :bug: Fix missing import

--- src/axi_adapter.sv
+++ src/axi_adapter.sv
@@ -17,6 +17,8 @@
 import std_cache_pkg::*;
 
 
+import nbdcache_pkg::*;
+
 module axi_adapter #(
         parameter int unsigned DATA_WIDTH          = 256,
         parameter logic        CRITICAL_WORD_FIRST = 0, // the AXI subsystem needs to support wrapping reads for this feature
