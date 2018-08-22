4159e56448daf7c4fcf56f11982a4bd1372a1bb7
Moritz Schneider <scmoritz@student.ethz.ch>
2018-08-22 17:57:31 +0200

    Add xilinx ips
      - Clock manager (clock wizard)
      - ddr3 controller/interface (mig)

---
+++ fpga/ips/xilinx_mig7_ddr3/tcl/run.tcl
@@ -0,0 +1,19 @@
+set partNumber $::env(XILINX_PART)
+set boardName  $::env(XILINX_BOARD)
+
+create_project xilinx_mig7_ddr3 . -force -part $partNumber
+set_property board_part $boardName [current_project]
+
+create_ip -name mig_7series -vendor xilinx.com -library ip -version 4.1 -module_name xilinx_mig7_ddr3
+exec cp mig_a.prj ./xilinx_mig7_ddr3.srcs/sources_1/ip/xilinx_mig7_ddr3/mig_a.prj
+set_property -dict [list CONFIG.XML_INPUT_FILE {mig_a.prj} CONFIG.RESET_BOARD_INTERFACE {Custom} CONFIG.MIG_DONT_TOUCH_PARAM {Custom} CONFIG.BOARD_MIG_PARAM {Custom}] [get_ips xilinx_mig7_ddr3]
+generate_target {instantiation_template} [get_files ./xilinx_mig7_ddr3.srcs/sources_1/ip/xilinx_mig7_ddr3/xilinx_mig7_ddr3.xci]
+update_compile_order -fileset sources_1
+generate_target all [get_files  ./xilinx_mig7_ddr3.srcs/sources_1/ip/xilinx_mig7_ddr3/xilinx_mig7_ddr3.xci]
+catch { config_ip_cache -export [get_ips -all xilinx_mig7_ddr3] }
+export_ip_user_files -of_objects [get_files ./xilinx_mig7_ddr3.srcs/sources_1/ip/xilinx_mig7_ddr3/xilinx_mig7_ddr3.xci] -no_script -sync -force -quiet
+create_ip_run [get_files -of_objects [get_fileset sources_1] ./xilinx_mig7_ddr3.srcs/sources_1/ip/xilinx_mig7_ddr3/xilinx_mig7_ddr3.xci]
+
+launch_runs -jobs 4 xilinx_mig7_ddr3_synth_1
+wait_on_run xilinx_mig7_ddr3_synth_1
+
