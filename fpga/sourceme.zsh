7807118bc2839981c972237993a2edeaf1842c3c
Moritz Schneider <scmoritz@student.ethz.ch>
2018-08-23 18:35:30 +0200

    Add arty-S7 FPGA

--- fpga/sourceme.zsh
+++ fpga/sourceme.zsh
@@ -1,10 +1,13 @@
 #!/bin/zsh 
 
-export BOARD="zybo"
+export BOARD="arty"
 
 if [ "$BOARD" = "zybo" ]; then
     export XILINX_PART="xc7z020clg400-1"
     export XILINX_BOARD="digilentinc.com:zybo-z7-20:part0:1.0"
+elif [ "$BOARD" = "arty" ]; then
+    export XILINX_PART="xc7s50csga324-1"
+    export XILINX_BOARD="digilentinc.com:arty-s7-50:part0:1.0"
 else
     echo "Board not supported!"
     exit
